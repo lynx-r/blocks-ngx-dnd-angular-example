274d126e51ee6e41ea6ef2f4378ff6b4cf2978d8
Aleksey <aleksey.popryaduhin@yahoo.com>
2018-08-02 11:42:24 +0300

    heroku hosting

--- src/app/apollo-client/apollo-client.module.ts
+++ src/app/apollo-client/apollo-client.module.ts
@@ -4,10 +4,12 @@ import {APOLLO_OPTIONS, ApolloModule} from 'apollo-angular';
 import {HttpClientModule} from '@angular/common/http';
 import {HttpLink, HttpLinkModule} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
+import {profile} from '../config/profile';
+import * as config from '../config/config.json';
 
 export function createApollo(httpLink: HttpLink) {
   return {
-    link: httpLink.create({uri: 'http://localhost:4567/graphql'}),
+    link: httpLink.create({uri: (<any>config)[profile].graphql}),
     cache: new InMemoryCache(),
     // defaultOptions: {
     // watchQuery: {
